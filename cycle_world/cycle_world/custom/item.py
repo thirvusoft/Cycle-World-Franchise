d9bc188cfd7d658046b64a384d60749f4edea0d3
Ubuntu <ubuntu@ip-172-26-7-243.ap-south-1.compute.internal>
2023-01-25 13:41:32 +0000

    fix: testing

--- cycle_world/cycle_world/custom/item.py
+++ cycle_world/cycle_world/custom/item.py
@@ -5,10 +5,10 @@ from frappe import _
 
 
 def validate(doc, event=None):
-	if(doc.get('transportation_cost') or (doc.get('shipping_cost') or 0)):
-		doc.additional_cost = (doc.get('transportation_cost') or 0) + (doc.get('shipping_cost') or 0)
+	doc.additional_cost = (doc.get('transportation_cost') or 0) + (doc.get('shipping_cost') or 0) + (doc.get('other_costs') or 0)
 	doc.standard_rate = (doc.get('standard_buying_cost') or 0) + (
-						(doc.get('standard_buying_cost') or 0)*(doc.get('ts_margin') or 0)/100) + (doc.get('additional_cost') or 0)
+						doc.get('additional_cost') or 0)
+	doc.standard_rate = doc.get('standard_rate') + doc.get('standard_rate')*(doc.get('ts_margin') or 0)/100 
 	doc.standard_rate = doc.get('standard_rate') - doc.get('standard_rate')*(doc.get('ts_discount_') or 0)/100 
 	if((doc.get('mrp') or 0) < (doc.get('standard_rate') or 0)):
 		frappe.msgprint(f'Standard Selling rate({doc.get("standard_rate")}) is greater than MRP rate({doc.get("mrp")}).')
--- cycle_world/cycle_world/custom/js/item.js
+++ cycle_world/cycle_world/custom/js/item.js
@@ -4,4 +4,33 @@ frappe.ui.form.on('Item',{
 		frm.set_value('item_name', frm.doc.brand_name)
 		frm.set_value('brand', frm.doc.brand_name)
 	},
+	standard_buying_cost(frm){
+		item_price(frm)
+	},
+	transportation_cost(frm){
+		item_price(frm)
+	},
+	shipping_cost(frm){
+		item_price(frm)
+	},
+	other_costs(frm){
+		item_price(frm)
+	},
+	ts_margin(frm){
+		item_price(frm)
+	},
+	ts_discount_(frm){
+		item_price(frm)
+	},
 })
+function item_price(frm){
+	var additional_cost = 0, selling_cost = 0;
+	additional_cost = frm.doc.transportation_cost + frm.doc.shipping_cost + frm.doc.other_costs
+	selling_cost = frm.doc.standard_buying_cost + additional_cost
+	selling_cost = selling_cost + (selling_cost * frm.doc.ts_margin) / 100
+	selling_cost = selling_cost - (selling_cost * frm.doc.ts_discount_) / 100
+	frm.set_value('additional_cost', additional_cost)
+	frm.set_value('standard_rate', selling_cost)
+	frm.refresh_field('additional_cost')
+	frm.refresh_field('standard_rate')
+}
