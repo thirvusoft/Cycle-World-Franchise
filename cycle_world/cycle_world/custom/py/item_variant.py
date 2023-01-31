fadbbbb3e83d64fa2307dc3ebb964373e4c40fea
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-01-31 16:01:23 +0530

    add prefix suffix for item creation name

--- cycle_world/cycle_world/custom/py/item_variant.py
+++ cycle_world/cycle_world/custom/py/item_variant.py
@@ -104,16 +104,13 @@ def make_variant_item_code(template_item_code, template_item_name, variant, dont
 			# frappe.throw(_('Invalid attribute {0} {1}').format(frappe.bold(attr.attribute),
 			# 	frappe.bold(attr.attribute_value)), title=_('Invalid Attribute'),
 			# 	exc=InvalidItemAttributeValueError)
-		print("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh")
-		print(item_attribute[0].abbr)
-		print(item_attribute[0])
 		abbr_or_value = (
-			cstr(attr.attribute_value) if item_attribute[0].numeric_values else item_attribute[0].abbr + item_attribute[0].prefix or "" + item_attribute[0].suffix or ""
+			cstr(attr.attribute_value) if item_attribute[0].numeric_values else (item_attribute[0].prefix or "") + item_attribute[0].abbr + (item_attribute[0].suffix or "")
 		)
 		abbreviations.append(abbr_or_value)
 
 		abbr_or_value_item_name = (
-			cstr(attr.attribute_value) if item_attribute[0].numeric_values else item_attribute[0].attribute_value + item_attribute[0].prefix or "" + item_attribute[0].suffix or "" if not item_attribute[0].show_only_abbreviation_in_item_name else item_attribute[0].abbr + item_attribute[0].prefix or "" + item_attribute[0].suffix or ""
+			cstr(attr.attribute_value) if item_attribute[0].numeric_values else  (item_attribute[0].prefix or "" ) + item_attribute[0].attribute_value + (item_attribute[0].suffix or "") if not item_attribute[0].show_only_abbreviation_in_item_name else (item_attribute[0].prefix or "") + item_attribute[0].abbr + (item_attribute[0].suffix or "")
 		)
 		abbr_for_item_name.append(abbr_or_value_item_name)
 
