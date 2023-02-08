a111f2bf68035473f78d076c293db1f331ac1450
sibikumarkuppusamy <sibi.kumar@thirvusoft.in>
2023-02-08 11:44:11 +0530

    feat:item quick entry

--- cycle_world/public/js/item_attribute_formatter.js
+++
@@ -1,9 +0,0 @@
-
-frappe.form.link_formatters['Item attribute Value'] = function(value, doc) {
-    console.log(value, doc)
-    // if(doc.employee_name && doc.employee_name !== value) {
-    //     return doc.employee_name + ': ' + value;
-    // } else {
-    //     return value;
-    // }
-}
